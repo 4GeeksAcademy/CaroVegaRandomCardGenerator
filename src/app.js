b9d45b14791bae67bdc6695061b2a11ba986e450
Adriana Carolina Vega Ahumada <carolinavegaahumada@gmail.com>
2023-08-26 02:29:13 +0000

    reloadv2

--- src/app.js
+++ src/app.js
@@ -25,10 +25,22 @@ window.onload = function() {
 };
 
 window.recarga = function recarga() {
-  location.reload();
+  //location.reload();
+  let suits = [
+    `<i class="bi bi-suit-spade-fill"></i>`,
+    `<i class="fa-solid fa-heart" style="color: #ff0000;"></i>`,
+    `<i class="bi bi-suit-club-fill"></i>`,
+    `<i class="bi bi-suit-diamond-fill"></i>`
+  ];
+  let valuesCard = ["A", "K", "Q", "j", 10, 9, 8, 7, 6, 5, 4, 3, 2];
+  let randomsuit = arrayRandom(suits);
+
+  document.getElementById("suit").innerHTML = randomsuit;
+  document.getElementById("suit1").innerHTML = randomsuit;
+  document.getElementById("valueC").textContent = arrayRandom(valuesCard);
 };
 
-setInterval(recarga, 10000);
+//setInterval(recarga, 10000);
 window.redimensionar = function redimensionar() {
   let alto = document.getElementById("alto").value;
   let ancho = document.getElementById("ancho").value;
